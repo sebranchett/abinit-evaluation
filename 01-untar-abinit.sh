e43c6e102b8f4ff9ef6fee8f858899083b3ade15
sebranchett <s.e.branchett@tudelft.nl>
2023-12-21 10:55:10 +0100

    more realistic time limit

--- 01-untar-abinit.sh
+++ 01-untar-abinit.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=untar-abinit
 #SBATCH --partition=compute
 #SBATCH --account=innovation
-#SBATCH --time=23:55:00
+#SBATCH --time=06:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1GB
